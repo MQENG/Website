15d3fbed293a814f8409fdad37881724d279cadc
collab112 <jadenchoi6@gmail.com>
2025-03-12 01:33:13 +1100

    dynamic scaling of modal + images

--- assets/events/loadEvents.js
+++ assets/events/loadEvents.js
@@ -25,10 +25,37 @@ function loadEventData() {
 
             });
 
+            const modals = document.querySelectorAll('.modal-box');
+
+            // Iterate through each modal
+            modals.forEach(modal => {
+
+                // the eventID is the data-id field of the HTML, and is the key in the events.json
+                const eventID = modal.dataset.id;
+
+                // Populate title, date, and location settings from JSON data
+                modal.querySelector('.modal-title').textContent = data[eventID]["title"] + " | " + data[eventID]["date"];
+                modal.querySelector('.modal-location').textContent = `Location: ${data[eventID]["location"]}`;
+                modal.querySelector('.modal-desc').textContent = data[eventID]["description"];
+
+                // Some events may not have images.
+                if (data[eventID]["img"]) {
+
+                    const img = document.createElement('img');
+                    img.src = 'assets/events/images/' + data[eventID]["img"];
+                    img.classList.add('modal-img');
+
+                    const content = modal.querySelector('.modal-content');
+                    const txt = modal.querySelector('.modal-text');
+
+                    content.insertBefore(img, txt);
+
+                }
+            });
         })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation: ', error);
-        });
+    .catch(error => {
+        console.error('There was a problem with the fetch operation: ', error);
+    });
 }
 
 loadEventData();
